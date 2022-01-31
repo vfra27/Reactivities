f5240d71a16201ff19616bdf2414183784918d8c
vfra27 <vfra2787@gmail.com>
2022-01-31 16:15:31 +0100

    End of section 10

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,7 @@ namespace Domain
 {
     public class Activity
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; }                
         public string Title { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; } 
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,10 +3,14 @@ import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 
 function App() {
@@ -15,7 +19,7 @@ function App() {
 
   return (
     <>
-
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route exact path='/' component={HomePage}></Route>
       <Route
         path={'/(.+)'}
@@ -24,9 +28,15 @@ function App() {
 
             <NavBar ></NavBar>
             <Container style={{ marginTop: '7em' }}>
-              <Route exact path='/activities' component={ActivityDashboard}></Route>
-              <Route path='/activities/:id' component={ActivityDetails}></Route>
-              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm}></Route>
+              
+              <Switch>{/*  just 1 route active at time */}
+                <Route exact path='/activities' component={ActivityDashboard}></Route>
+                <Route path='/activities/:id' component={ActivityDetails}></Route>
+                <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm}></Route>
+                <Route path='/errors' component={TestErrors} />
+                <Route path='/server-error' component={ServerError} />
+                <Route component={NotFound} />                
+              </Switch>
             </Container>
           </>
         )}
